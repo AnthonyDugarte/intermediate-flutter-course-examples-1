927bc297d4cfbf275794fdf8ee70ab43677719cd
Anthony Dugarte <toonny1998@gmail.com>
2019-10-09 18:38:11 -0400

    Switch button example finished

--- lib/main.dart
+++ lib/main.dart
@@ -23,13 +23,28 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool _value = false;
+  _triggerValue(bool value) => setState(() => _value = value);
+
   @override
   Widget build(BuildContext context) => Container(
         padding: EdgeInsets.all(28.0),
-        child: Column(
-          children: <Widget>[
-            Text("Quieres este elemento?"),
-          ],
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text("Quieres este elemento?"),
+              Switch(
+                value: _value,
+                onChanged: _triggerValue,
+                activeColor: Colors.blueGrey,
+                inactiveTrackColor: Colors.amber,
+              ),
+              RaisedButton(
+                child: Text("Hehe"),
+                onPressed: () => debugPrint("$_value"),
+              ),
+            ],
+          ),
         ),
       );
 }
