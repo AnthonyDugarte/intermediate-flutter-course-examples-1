da08bcd797edb6639bcdc5f5839046303b982465
Anthony Dugarte <toonny1998@gmail.com>
2019-10-09 20:37:27 -0400

    Custom drawer

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,46 @@ class MyAppLayout extends StatelessWidget {
         appBar: AppBar(
           title: Text("$titleString Example"),
         ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              UserAccountsDrawerHeader(
+                accountName: Text("Anthony Dugarte"),
+                accountEmail: Text("toonny1998@gmail.com"),
+                currentAccountPicture: CircleAvatar(
+                  backgroundColor: Colors.black,
+                  backgroundImage: NetworkImage(
+                      "https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/6/1/c/a/61ca1dcbc2cdda2af430927f4fe4b98c.jpg"),
+                  // child: Text("AD"),
+                ),
+                otherAccountsPictures: <Widget>[
+                  CircleAvatar(
+                    backgroundColor: Colors.blueGrey,
+                    child: Text("IG"),
+                  ),
+                ],
+              ),
+              ListTile(
+                title: Text("Opción 1"),
+                onTap: () {},
+                trailing: Icon(Icons.brightness_2),
+              ),
+              ListTile(
+                title: Text("Op 2"),
+                trailing: Icon(Icons.atm),
+                onTap: () {},
+              ),
+              ListTile(
+                title: Text("Cerrar"),
+                trailing: Icon(Icons.close),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
         body: MyApp(),
       );
 }
@@ -23,23 +63,8 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  double _val = 0.0;
-
-  void _setVal(double val) => setState(() => _val = val);
-
   @override
   Widget build(BuildContext context) => Container(
         padding: EdgeInsets.all(28),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Slider(
-                value: _val,
-                onChanged: _setVal,
-              ),
-              Text("${(_val * 100).round()}"),
-            ],
-          ),
-        ),
       );
 }
